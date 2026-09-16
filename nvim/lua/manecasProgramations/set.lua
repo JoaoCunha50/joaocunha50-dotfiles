165be8ea799a4a1510410d4f3a16d18777a25586
João Cunha <131183584+JoaoCunha50@users.noreply.github.com>
2026-09-16 22:48:39 +0000

    Enable sign column and smooth scrolling in Neovim

--- nvim/lua/manecasProgramations/set.lua
+++ nvim/lua/manecasProgramations/set.lua
@@ -12,7 +12,6 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 
 vim.opt.swapfile = false
@@ -26,9 +25,9 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "no"
+vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
+opt.smoothscroll = true
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = ""
